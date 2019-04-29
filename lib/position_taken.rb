cedb5e81fae53f2f9148a3a53dce3cd26d2471a1
Nichole Battleday <battleday@gmail.com>
2019-04-29 03:23:27 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,5 @@
-def position_taken(board, index)
-  if board[index] == " " || baord[index] == "" || board[index] == nil
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
     return false
   else
     return true
